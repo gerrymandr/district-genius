ae5b10150b8258f3feabab6fc0e4db27b7475a9a
removedporn <86824510+removedporn@users.noreply.github.com>
2022-05-09 13:02:49 +0800

    Delete static directory

--- static/force-https.js
+++
@@ -1,4 +0,0 @@
-// make /login and /register happen in HTTPS
-if (window.location.protocol === "http:") {
-  window.location.href = 'https://district-genius.herokuapp.com' + window.location.pathname;
-}
